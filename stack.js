b4110f1bc041954b27ba824ee6fc7b4b79e386b3
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-06 20:53:02 +0900

    feat: class형 stack 구현

---
+++ stack.js
@@ -0,0 +1,70 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 0;
+  }
+
+  push(data) {
+    let newNode = new Node(data, null);
+
+    if (this.isEmpty()) {
+      this.top = newNode;
+      this.size++;
+
+      return;
+    }
+
+    newNode.next = this.top;
+    this.top = newNode;
+
+    this.size++;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return;
+    }
+    let cur = this.top;
+    this.top = this.top.next;
+
+    this.size--;
+    console.log(cur.data);
+  }
+
+  print() {
+    let cur = this.top;
+
+    console.log(`크기 :: ${this.getSize()}`);
+    while (cur != null) {
+      console.log(cur.data);
+      cur = cur.next;
+    }
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  isEmpty() {
+    return !this.size;
+  }
+}
+
+const stack = new Stack();
+
+stack.push(5);
+stack.print();
+stack.push(4);
+stack.print();
+stack.push(3);
+stack.print();
+
+stack.pop();
+stack.print();
