0ac1e6f5b8d1fc371379f73d2e8761401b6675a8
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-06 21:22:14 +0900

    feat: 함수형 queue 구현

---
+++ queue.js
@@ -0,0 +1,14 @@
+let queue = [];
+
+queue.push("월");
+queue.push("화");
+queue.push("수");
+queue.push("목");
+queue.push("금");
+queue.push("토");
+
+console.log(queue);
+
+console.log(queue.shift());
+console.log(queue.shift());
+console.log(queue);
